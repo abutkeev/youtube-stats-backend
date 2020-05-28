54eb4d240576d258e2f4bfcac3247b7776d60c12
Alexey V. Butkeev <abutkeev@abutkeev.ru>
2020-05-28 19:25:19 +0300

    update config example

--- etc/config_example.php
+++ etc/config_example.php
@@ -1,4 +1,12 @@
 <?php
-final class Config {
+final class Config
+{
     const YOUTUBE_KEY = 'DEVELOPER KEY';
+    const YOUTUBE_PUSH_CALLBACK = 'URL';
+
+    const DB_LOGIN = 'user';
+    const DB_PASSWORD = 'passwd';
+    const DB_HOST = 'host';
+    const DB_NAME = 'name';
+
 }
